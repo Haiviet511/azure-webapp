c1824d8f8b88fa53f1313a154ec9ccc1aeb96e21
Haiviet511 <haiviet411@gmail.com>
2025-09-06 18:54:49 +0700

    Revert "Update code"

--- src/main/java/com/example/azure_webapp/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.azure_webapp.repository;
-
-import com.example.azure_webapp.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {}
